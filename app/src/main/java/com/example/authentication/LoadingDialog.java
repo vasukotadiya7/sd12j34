980b8faee788b31a1754fbf8174fb0227eaf1655
vasukotadiya7 <vasukotadiya878op@gmail.com>
2023-02-10 10:25:01 +0530

    Authentication App

---
+++ app/src/main/java/com/example/authentication/LoadingDialog.java
@@ -0,0 +1,29 @@
+package com.example.authentication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+
+class LoadingDialog {
+
+    private  Activity activity;
+    private AlertDialog dialog;
+    LoadingDialog(Activity myActivity){
+        activity =myActivity;
+    }
+
+    void  startLoadingDialog(){
+        AlertDialog.Builder builder =new AlertDialog.Builder(activity);
+
+        LayoutInflater inflater=activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.custom_dialog,null));
+        builder.setCancelable(true);
+        dialog=builder.create();
+        dialog.show();
+    }
+
+    void dismissDialog(){
+        dialog.dismiss();
+    }
+
+}
